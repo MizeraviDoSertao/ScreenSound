6f85e6638983239d5a74b51b54c742d830f4a036
pedro <pedro@GAMER>
2025-08-22 10:32:40 -0300

    Protege a manipulação das avaliações

--- ScreenSound/Models/Avaliacao.cs
+++ ScreenSound/Models/Avaliacao.cs
@@ -12,7 +12,24 @@ internal class Avaliacao
 
     public static Avaliacao Parse(string texto)
     {
-        int nota1 = int.Parse(texto);
-        return new Avaliacao(nota1);
+        int nota = int.Parse(texto);
+        return new Avaliacao(nota);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null) return false;
+        if (obj is not Avaliacao other) return false;
+        return Nota.Equals(other.Nota);
+    }
+
+    public override int GetHashCode()
+    {
+        return Nota.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return Nota.ToString();
     }
 }
